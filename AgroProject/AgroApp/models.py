3c02dc5034b4574ffe0f1710f062b388b54d299f
Abrorpython <78807087+Abrorpython@users.noreply.github.com>
2021-03-05 20:51:52 +0500

    .

--- AgroProject/AgroApp/models.py
+++ AgroProject/AgroApp/models.py
@@ -8,19 +8,18 @@ class User(models.Model):
         ("2", "Yuridik shaxs"),
     )
 
-    UserNameOne = models.CharField(max_length=50, choices=ChoiseOne, null=False, blank=False)
-    UserNameTwo = models.CharField(max_length=50, null=False, blank=False)
-    UserEmail = models.EmailField(max_length=100, null=False, blank=False)
-    UserPassword = models.CharField(max_length=100, null=False, blank=False)
-    UserPhone = models.CharField(max_length=100, null=False, blank=False)
-
     ChoiseTwo = (
         ("1", True),
         ("2", False)
     )
 
+    UserNameOne = models.CharField(max_length=50, choices=ChoiseOne, null=False, blank=False)
+    UserNameTwo = models.CharField(max_length=50, null=False, blank=False)
+    UserEmail = models.EmailField(max_length=100, null=False, blank=False)
+    UserPassword = models.CharField(max_length=100, null=False, blank=False)
+    UserPhone = models.CharField(max_length=100, null=False, blank=False)
     UserOferta = models.BooleanField(max_length=50, choices=ChoiseTwo, null=False, blank=True)
-    UserTime = models.DateTimeField(blank=False,null=False)
+    UserTime = models.DateTimeField(null=False)
 
     def __str__(self):
-        return "%s  -  %s" % (self.UserNameOne, self.ChoiseTwo)
+        return self.UserNameOne
